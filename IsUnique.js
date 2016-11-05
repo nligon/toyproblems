1cd5a0a846f8688238ac1fab2209d6cd15cf91e4
Nathanael Ligon <nathanaelligon@Es-iMac.local>
2016-11-05 12:26:18 -0700

    Implement constant time comlexity solution for isUnique

--- IsUnique.js
+++ IsUnique.js
@@ -1,5 +1,17 @@
 // Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
-var isUnique = function() {
-	
+// O(N), constant time complexity solution using hash table.
+var isUnique = function(string) {
+	var obj = {};
+	for (var i = 0; i < string.length, i++) {
+		if (obj[i]) {
+			return false;
+		} else {
+			obj[i] = true;
+		}
+	}
+	return true;
 }
+
+var someString = 'abbc';
+console.log(isUnique(someString));
