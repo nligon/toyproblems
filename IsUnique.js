93159533b5613527c28b3fb6361b70edbec6d046
Nathanael Ligon <nathanaelligon@Es-iMac.local>
2016-11-05 13:23:54 -0700

    Implement non-data structure, exponential-time-complexity solution for isUnique

--- IsUnique.js
+++ IsUnique.js
@@ -2,16 +2,25 @@
 
 // O(N), constant time complexity solution using hash table.
 var isUnique = function(string) {
-	var obj = {};
-	for (var i = 0; i < string.length; i++) {
-		if (obj[i]) {
-			return false;
-		} else {
-			obj[i] = true;
-		}
-	}
-	return true;
-}
+  var obj = {};
+  for (var i = 0; i < string.length; i++) {
+    if (obj[i]) {
+      return false;
+    } else {
+      obj[i] = true;
+    }
+  }
+  return true;
+};
 
-var someString = 'abbc';
-console.log(isUnique(someString));
+// O(N^2), exponential time complexity solution, without additional data structures.
+var isUnique = function(string) {
+  for (var i = 0; i < string.length; i++) {
+    for (var j = i; j < string.length; j++) {
+      if (string[i] === string[i + 1]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
