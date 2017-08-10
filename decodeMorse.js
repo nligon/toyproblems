19b970341e39af980d0433c31ebc484fd8588348
Nathanael Ligon <nathanaelligon@gmail.com>
2017-08-10 13:48:52 -0700

    Heavily refine decodeMorse solution to comply with additional unit testing

--- decodeMorse.js
+++ decodeMorse.js
@@ -10,8 +10,7 @@ Your task is to implement a function decodeMorse(morseCode), that would take the
 
 For example:
 
-decodeMorse('.... . -.--   .--- ..- -.. .')
-//should return "HEY JUDE"
+decodeMorse('.... . -.--   .--- ..- -.. .'
 The Morse code table is preloaded for you as a dictionary, feel free to use it. In CoffeeScript, C++, JavaScript, PHP, Python, Ruby and TypeScript, the table can be accessed like this: MORSE_CODE['.--'], in Java it is MorseCode.get('.--'), in C# it is MorseCode.Get('.--'), in Haskell the codes are in a Map String String and can be accessed like this: morseCodes ! ".--", in Elixir it is morse_codes variable.
 
 All the test strings would contain valid Morse code, so you may skip checking for errors and exceptions. In C#, tests will fail if the solution code throws an exception, please keep that in mind. This is mostly because otherwise the engine would simply ignore the tests, resulting in a "valid" solution.
@@ -21,7 +20,6 @@ Good luck!
 
 const MORSE_CODE = { '-.-.--': '!', '.-..-.': '"', '...-..-': '$', '.-...': '&', '.----.': '\'', '-.--.': '(', '-.--.-': ')', '.-.-.': '+', '--..--': ',', '-....-': '-', '.-.-.-': '.', '-..-.': '/', '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', '---...': ':', '-.-.-.': ';', '-...-': '=', '..--..': '?', '.--.-.': '@', '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z', '..--.-': '_', '...---...': 'SOS' };
 
-// naive ES6 implementation using Array.prototype.map()
 const decodeMorse1 = morseCode => {
   let results = '';
   let morseStr = morseCode.split(' ').map(val => MORSE_CODE[val] || ' ')
@@ -30,14 +28,18 @@ const decodeMorse1 = morseCode => {
   });
   return results;
 };
-
-// naive ES6 implementation using Array.prototype.map()
-const decodeMorse = morseCode => {
-  // let results = '';
+const decodeMorse2 = morseCode => {
   return morseCode.split(' ').map(val => {
     return MORSE_CODE[val] || ' '
   }).reduce((tot, val, i, arr) => arr[i] !== ' ' || arr[i - 1] === ' ' ? tot += val : tot += '', '');
 };
 
-// tests
-console.log(decodeMorse('.... . -.--   .--- ..- -.. .')); // 'HEY JUDE'
+const decodeMorse3 = morseCode => 
+  morseCode.split(' ')
+  .reduce((tot, val, i, arr) =>
+    (val) ? (tot += MORSE_CODE[val]) :
+    (arr[i - 1] === '') ? (tot) :
+    (tot += ' '), '')
+  .trim();
+
+console.log('*' + decodeMorse3('     .... . -.--   .--- ..- -.. .     ') + '*');
