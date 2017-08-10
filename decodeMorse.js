f66ac7935aa868ebe0f440597dd1399618475678
Nathanael Ligon <nathanaelligon@gmail.com>
2017-08-10 13:54:43 -0700

    Clean up morseCode implementation.

--- decodeMorse.js
+++ decodeMorse.js
@@ -20,26 +20,7 @@ Good luck!
 
 const MORSE_CODE = { '-.-.--': '!', '.-..-.': '"', '...-..-': '$', '.-...': '&', '.----.': '\'', '-.--.': '(', '-.--.-': ')', '.-.-.': '+', '--..--': ',', '-....-': '-', '.-.-.-': '.', '-..-.': '/', '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', '---...': ':', '-.-.-.': ';', '-...-': '=', '..--..': '?', '.--.-.': '@', '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z', '..--.-': '_', '...---...': 'SOS' };
 
-const decodeMorse1 = morseCode => {
-  let results = '';
-  let morseStr = morseCode.split(' ').map(val => MORSE_CODE[val] || ' ')
-  morseStr.forEach((val, i) => {
-    (morseStr[i] !== ' ' || morseStr[i - 1] === ' ') && (results += val);
-  });
-  return results;
-};
-const decodeMorse2 = morseCode => {
-  return morseCode.split(' ').map(val => {
-    return MORSE_CODE[val] || ' '
-  }).reduce((tot, val, i, arr) => arr[i] !== ' ' || arr[i - 1] === ' ' ? tot += val : tot += '', '');
-};
-
-const decodeMorse3 = morseCode => 
+const decodeMorse = morseCode => 
   morseCode.split(' ')
   .reduce((tot, val, i, arr) =>
-    (val) ? (tot += MORSE_CODE[val]) :
-    (arr[i - 1] === '') ? (tot) :
-    (tot += ' '), '')
-  .trim();
-
-console.log('*' + decodeMorse3('     .... . -.--   .--- ..- -.. .     ') + '*');
+    val ? tot += MORSE_CODE[val] : !arr[i - 1] ? tot : tot += ' ', '').trim();
