0f9ed8c6613fcdce38fe747e6d04342776f79d36
Nathanael Ligon <nathanaelligon@gmail.com>
2017-08-09 22:03:12 -0700

    deleteNth naive implementation

--- deleteNth.js
+++ deleteNth.js
@@ -1,6 +1,20 @@
 // Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
 
-
-function deleteNth(arr,x){
-  // ...
+// naive implementation
+function deleteNth(arr, x) {
+  var hash = {};
+  var results = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (hash[arr[i]] === undefined) {
+        hash[arr[i]] = 1;
+        results.push(arr[i]);
+    } else if (hash[arr[i]] < x) {
+        results.push(arr[i])
+        hash[arr[i]]++;
+    }
+  }
+  return results;
 }
+
+
+console.log(deleteNth([20,37,20,21], 1)) // [20,37,21]
